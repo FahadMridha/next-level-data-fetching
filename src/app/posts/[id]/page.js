12da24b6508755703544328d308b380d81e1eb4d
fahad Mridha <sohelcse16@gmail.com>
2024-02-02 02:35:15 +0600

    api route create

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -1,26 +1,41 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
-const DetailPage = async({params}) => {
-    const id = params.id 
-    const res = await fetch (`http://localhost:5000/posts/${id}`)
-    const post = await res.json ()
-    return (
-        <div>
-                                <div key={post.id} className="card bg-gray-100 shadow-xl my-5 w-[70%] mx-auto">
-                        <div className="card-body">
-                            <h2 className="card-title">{post.title}</h2>
-                            <p>Des: {post.description}</p>
-                            <p>Likes: {post.likeCount}</p>
-                            <div className="card-actions justify-end">
-                            <Link href="/posts">
-                            <button className="btn btn-primary">Back Post</button>
-                            </Link>
-                            </div>
-                        </div>
-                    </div>
+export async function generateStaticParams() {
+  const res = await fetch("http://localhost:5000/posts");
+  const posts = await res.json();
+  const ids = posts.map((post) => {
+    return {
+      id: post.id + "",
+    };
+  });
+  // console.log(ids);
+  return ids;
+}
+
+const DetailPage = async ({ params }) => {
+  const id = params.id;
+  const res = await fetch(`http://localhost:5000/posts/${id}`);
+  const post = await res.json();
+  return (
+    <div>
+      <div
+        key={post.id}
+        className="card bg-gray-100 shadow-xl my-5 w-[70%] mx-auto"
+      >
+        <div className="card-body">
+          <h2 className="card-title">{post.title}</h2>
+          <p>Des: {post.description}</p>
+          <p>Likes: {post.likeCount}</p>
+          <div className="card-actions justify-end">
+            <Link href="/posts">
+              <button className="btn btn-primary">Back Post</button>
+            </Link>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default DetailPage;
--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
+import styles from "./Posts.module.css"
+
 
 const PostPage =async () => {
         const res = await fetch('http://localhost:5000/posts',{
@@ -9,7 +11,7 @@ const PostPage =async () => {
 
     return (
         <div className='w-full'>
-            <h2>Total post :{posts.length} </h2>
+            <h2 className={styles.header_text}>Total Number of post :{posts.length} </h2>
             {
                 posts.map((post) => (
                     <div key={post.id} className="card bg-gray-100 shadow-xl my-5 w-[70%] mx-auto">
